6f4d1f79bfaa179ce72fdbdbfb1696fa46c3f4e0
Ben Gottlieb <ben@standalone.com>
2019-11-10 11:01:09 -0600

    made tweet components public

--- Sources/SwifterTweet.swift
+++ Sources/SwifterTweet.swift
@@ -18,108 +18,108 @@ extension Swifter {
 			return decoder
 		}
 
-		var id_str = ""
-		var is_quote_status = false
-		var source = ""
-		var user: User
-		var lang = ""
-		var favorite_count: Int64
-		var retweet_count: Int64
-		var favorited = false
-		var id: Int64
-		var retweeted = false
-		var possibly_sensitive: Bool? = false
-		var text = ""
-		var entities: TweetEntities
-		var extended_entities: ExtendedEntity?
-		var truncated = false
-		var created_at: Date?
+		public var id_str = ""
+		public var is_quote_status = false
+		public var source = ""
+		public var user: User
+		public var lang = ""
+		public var favorite_count: Int64
+		public var retweet_count: Int64
+		public var favorited = false
+		public var id: Int64
+		public var retweeted = false
+		public var possibly_sensitive: Bool? = false
+		public var text = ""
+		public var entities: TweetEntities
+		public var extended_entities: ExtendedEntity?
+		public var truncated = false
+		public var created_at: Date?
 
 		
-		struct User: Codable {
-			var id: UInt64
-			var name = ""
+		public struct User: Codable {
+			public var id: UInt64
+			public var name = ""
 
-			var id_str = ""
-			var protected = false
-			var profile_sidebar_border_color: String? = ""
-			var profile_link_color: String? = ""
-			var favourites_count = 0.0
-			var profile_background_color: String? = ""
-			var profile_sidebar_fill_color: String? = ""
-			var profile_background_tile = false
-			var profile_image_url_https: String? = ""
-			var friends_count = 0
-			var entities: UserEntities?
-			var profile_background_image_url_https: String? = ""
-			var profile_image_url: String? = ""
-			var statuses_count = 0
-			var following = false
-			var default_profile = false
-			var listed_count = 0.0
-			var geo_enabled = false
-			var profile_use_background_image = false
-			var screen_name: String? = ""
-			var description: String? = ""
-			var followed_by = false
-			var notifications = false
-			var contributors_enabled = false
-			var followers_count = 0
-			var has_extended_profile = false
-			var verified = false
-			var location: String? = ""
-			var created_at: Date?
-			var follow_request_sent = false
-			var profile_background_image_url: String? = ""
-			var profile_text_color: String? = ""
+			public var id_str = ""
+			public var protected = false
+			public var profile_sidebar_border_color: String? = ""
+			public var profile_link_color: String? = ""
+			public var favourites_count = 0.0
+			public var profile_background_color: String? = ""
+			public var profile_sidebar_fill_color: String? = ""
+			public var profile_background_tile = false
+			public var profile_image_url_https: String? = ""
+			public var friends_count = 0
+			public var entities: UserEntities?
+			public var profile_background_image_url_https: String? = ""
+			public var profile_image_url: String? = ""
+			public var statuses_count = 0
+			public var following = false
+			public var default_profile = false
+			public var listed_count = 0.0
+			public var geo_enabled = false
+			public var profile_use_background_image = false
+			public var screen_name: String? = ""
+			public var description: String? = ""
+			public var followed_by = false
+			public var notifications = false
+			public var contributors_enabled = false
+			public var followers_count = 0
+			public var has_extended_profile = false
+			public var verified = false
+			public var location: String? = ""
+			public var created_at: Date?
+			public var follow_request_sent = false
+			public var profile_background_image_url: String? = ""
+			public var profile_text_color: String? = ""
 			
-			struct UserEntities: Codable  {
-				var description: Entity?
-				var url: Entity?
+			public struct UserEntities: Codable  {
+				public var description: Entity?
+				public var url: Entity?
 			}
 		}
 		
-		struct TweetEntities: Codable {
-			var hashtags: [Entity]
-			var symbols: [Entity]
-			var urls: [Entity]
-			var user_mentions: [Entity]
+		public struct TweetEntities: Codable {
+			public var hashtags: [Entity]
+			public var symbols: [Entity]
+			public var urls: [Entity]
+			public var user_mentions: [Entity]
 		}
 
 	}
 	
-	struct Entity: Codable {
-		var display_url: String? = ""
-		var indices: [Int]? = []
-		var expanded_url: String? = ""
-		var url: String? = ""
+	public struct Entity: Codable {
+		public var display_url: String? = ""
+		public var indices: [Int]? = []
+		public var expanded_url: String? = ""
+		public var url: String? = ""
 	}
 
-	struct ExtendedEntity: Codable {
-		var media: [Media]?
+	public struct ExtendedEntity: Codable {
+		public var media: [Media]?
 		
-		struct Media: Codable {
-			var media_url_https: String! = ""
-			var id: UInt64 = 0
-			var expanded_url: String! = ""
-			var media_url: String! = ""
-			var id_str = ""
-			var display_url: String! = ""
-			var type = ""
-			var sizes: Sizes
-			var indices: [Int] = []
-			var url = ""
+		public struct Media: Codable {
+			public var media_url_https: String! = ""
+			public var id: UInt64 = 0
+			public var expanded_url: String! = ""
+			public var media_url: String! = ""
+			public var id_str = ""
+			public var display_url: String! = ""
+			public var type = ""
+			public var sizes: Sizes
+			public var indices: [Int] = []
+			public var url = ""
 			
-			struct Sizes: Codable {
-				var large: Size?
-				var medium: Size?
-				var small: Size?
-				var thumb: Size?
+			public struct Sizes: Codable {
+				public var large: Size?
+				public var medium: Size?
+				public var small: Size?
+				public var thumb: Size?
 				
 				struct Size: Codable {
-					var h: Int
-					var w: Int
-					var resize: String
+					public var h: Int
+					public var w: Int
+					public var resize: String
 				}
 			}
 		}
